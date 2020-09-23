f2196301d7eb877fb39572125e25392b9ad0ba82
Danilo Moura <danilolmoura@gmail.com>
2020-09-23 19:52:10 -0300

    list places around me

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:iservice_mobile/screens/search.dart';
 import 'package:iservice_mobile/services/geolocator_service.dart';
+import 'package:iservice_mobile/services/places_services.dart';
 import 'package:provider/provider.dart';
+
+import 'models/place.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +13,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final locatorService = GeolocatorService();
+  final placesServices = PlacesServices();
 
   @override
   Widget build(BuildContext context) {
-    return FutureProvider(
-      create: (context) => locatorService.getLocation(),
+    return MultiProvider(
+      providers: [
+        FutureProvider(
+          create: (context) => locatorService.getLocation(),
+        ),
+        ProxyProvider<Position, Future<List<Place>>>(
+          update: (context, position, places) {
+            return (position != null)
+                ? placesServices.getPlacess(
+                    position.latitude, position.longitude)
+                : null;
+          },
+        )
+      ],
       child: MaterialApp(
         title: 'Iservice App',
         theme: ThemeData(
--- lib/services/places_services.dart
+++ lib/services/places_services.dart
@@ -3,13 +3,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class PlacesServices {
-  final key = 'AIzaSyACEA4v57gd4lMmfg1fwWcmHKXXLiAFZZU';
+  final key = '<YOUKEYHERE>';
 
   Future<List<Place>> getPlacess(double lat, double lng) async {
     var response = await http.get(
-        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=parking&rankby=distance&key=$key');
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=parking&rankby=distances');
     var json = convert.jsonDecode(response.body);
     var jsonResults = json['results'] as List;
-    jsonResults.map((place) => Place.fromJson(place)).toList();
+    return jsonResults.map((place) => Place.fromJson(place)).toList();
   }
 }
